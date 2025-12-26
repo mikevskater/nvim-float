f1a09fb41200398982bd63dd522859c88586a3ac
mikevskater <mikevskater@hotmail.com>
2025-12-26 00:49:50 -0500

    (0.0.0): add nvim-float plugin entry point with version and demo commands

---
+++ plugin/nvim-float.lua
@@ -0,0 +1,24 @@
+-- nvim-float plugin entry point
+-- This file is automatically loaded by Neovim
+
+-- Prevent loading twice
+if vim.g.loaded_nvim_float then
+  return
+end
+vim.g.loaded_nvim_float = 1
+
+-- Version command
+vim.api.nvim_create_user_command("NvimFloatVersion", function()
+  local nvim_float = require("nvim-float")
+  vim.notify(string.format("nvim-float version: %s", nvim_float.version), vim.log.levels.INFO)
+end, {
+  desc = "Show nvim-float version",
+})
+
+-- Demo command for testing
+vim.api.nvim_create_user_command("NvimFloatDemo", function()
+  local nvim_float = require("nvim-float")
+  nvim_float.demo()
+end, {
+  desc = "Show nvim-float demo window",
+})
